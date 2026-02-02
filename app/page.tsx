9dbe7960e9e80f584d6956a79856feea34311272
britto1996 <btm15037@gmail.com>
2026-02-02 10:29:03 +0400

    Feat: Nextjs application started.

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <button className="btn btn-primary border-0">
+        Deploy
+      </button>
+    </div>
+  );
+}
